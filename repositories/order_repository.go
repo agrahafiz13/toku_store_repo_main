188f7daeb25bfdbb11a227e81cd1f048426629cd
agrahafiz13 <agrahafiz2@gmail.com>
2026-05-22 19:34:16 +0700

    feat: Order Repo
    membuat fungsi crud data dari database

---
+++ repositories/order_repository.go
@@ -0,0 +1,49 @@
+package repositories
+
+import (
+	"github.com/agrahafiz13/toku_store_backend/config"
+	"github.com/agrahafiz13/toku_store_backend/models"
+)
+
+func CreateOrder(
+	order *models.Order,
+) error {
+
+	return config.DB.Create(order).Error
+}
+
+func CreateOrderItem(
+	item *models.OrderItem,
+) error {
+
+	return config.DB.Create(item).Error
+}
+
+func GetOrdersByUserID(
+	userID uint,
+) ([]models.Order, error) {
+
+	var orders []models.Order
+
+	err := config.DB.
+		Preload("Items").
+		Where("user_id = ?", userID).
+		Order("id DESC").
+		Find(&orders).Error
+
+	return orders, err
+}
+
+func GetOrderByID(
+	id uint,
+) (*models.Order, error) {
+
+	var order models.Order
+
+	err := config.DB.
+		Preload("Items").
+		Preload("Items.Product").
+		First(&order, id).Error
+
+	return &order, err
+}
